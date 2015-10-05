f4fd635174b03453a6c504f09ec528804eaa938e
nikolay.torbev <ntorbev@gmail.com>
2015-10-05 11:37:16 +0300

    changes for heroku favicon update

--- server/config/express.js
+++ server/config/express.js
@@ -1,5 +1,5 @@
 var path = require('path'),
-    favicon = require('static-favicon'),
+    favicon = require('serve-favicon'),
     logger = require('morgan'),
     cookieParser = require('cookie-parser'),
     bodyParser = require('body-parser'),
@@ -9,7 +9,7 @@ module.exports = function(app, express) {
     app.set('view engine', 'jade');
 
     app.set('views', path.join(__dirname, '../../views'));
-    app.use(favicon());
+    app.use(favicon(__dirname + '../../../public/images/wendy-favicon.png'));
 
     app.use(logger('dev'));
     app.use(bodyParser.json());
